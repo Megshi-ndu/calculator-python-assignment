11fa02e215ffc7f34cbdbdbad8f5c7c09a64fca2
Megshi-ndu <megshibeta2002@gmail.com>
2025-08-05 01:23:47 +0300

    Added Calculator assgn.py

---
+++ Calculator assgn.py
@@ -0,0 +1,4 @@
+a, b = 10, 5
+#Output
+print("10 + 5 =" , a + b)
+# This is a simple arithmetic addition operation for a calculator a and b represent integers, numeric int data type
---
+++ calculator.py
@@ -0,0 +1,2 @@
+print("10 + 5 =", 15)
+# This is a simple addition operation for a calculator
